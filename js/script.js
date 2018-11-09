2dd04fd869757149af8de4302c44d7a1847e865f
nandiniicm <nandinicm239@gmail.com>
2018-11-09 12:56:52 +0530

    day3 added

--- js/script.js
+++ js/script.js
@@ -17,4 +17,63 @@ function Read(){
         console.log("Not Eligible");
         alert("Not Eligible");
     }
+    var op=document.getElementById("district");
+var district=op.options[op.selectedIndex].value;
+    console.log(district);
     }
+
+    
+
+
+    function Largest()
+{
+    var Num1= document.getElementById("getn1").value;
+        
+        var Num2= document.getElementById("getn2").value;
+        var Num3= document.getElementById("getn3").value;
+
+        var x=parseInt(Num1);
+        var y=parseInt(Num2);
+        var z=parseInt(Num3);
+        if(x>y && x>z)
+        {
+            console.log(x);
+        }
+        else if(y>x && y>z)
+        {
+            console.log(y);
+        }
+else (z>x && z>y)
+{
+    console.log(z);
+}
+}
+
+function smallest()
+{
+    var Num1= document.getElementById("getn1").value;
+        
+        var Num2= document.getElementById("getn2").value;
+        var Num3= document.getElementById("getn3").value;
+
+        var x=parseInt(Num1);
+        var y=parseInt(Num2);
+        var z=parseInt(Num3);
+        if(x<y && x<z)
+        {
+            console.log(x);
+        }
+        else if(y<x && y<z)
+        {
+            
+            console.log(y);
+        }
+else if(z<x && z<y)
+{
+    
+    console.log(z);
+
+}
+else
+console.log("error");
+}
